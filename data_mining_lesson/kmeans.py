5680e9114bbea2dde49040a8398d7e1d93b201c3
achieve0410 <achieve0410@gmail.com>
2019-05-16 17:39:20 +0900

    delete hw

---
+++ data_mining_lesson/kmeans.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def loadDataSet(fileName):
+    dataMat = []
+    fr = open(fileName)
+    for line in fr.readlines():
+        curLine = line.strip().split('\t')
+        fltLine = map(float, curLine)
+        dataMat.append(fltLine)
+    return dataMat
+
